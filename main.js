465be3cde24b685619c98afb45389366d26272d3
Te Rahbæk <tera0002@stud.ek.dk>
2026-02-25 23:58:09 +0100

    aflever

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Console log loaded...");
+
+// brug følgende til oprettelse af array med elementer
+// [{"category":"Accessories"},{"category":"Apparel"},{"category":"Footwear"},{"category":"Free Items"},{"category":"Personal Care"},{"category":"Sporting Goods"}]
